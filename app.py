b932b0f8d7f0b8aade318e7a6b26246d73f7da99
marnoack <marnoack@yahoo.com>
2026-04-16 08:49:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -242,8 +242,8 @@ def get_receipt_content(row, selected_period, common_area_consumption, COEFFICIE
     </table>
     <hr style="margin: 10px 0;">
     <table style="font-size: 0.9em;">
-        <tr class="bg-black border-tb-white"><td>PRESUPUESTO TOTAL DEL MES:</td><td class="text-right p-5">S/. {monthly_budget:.2f}</td></tr>
-        <tr class="bg-black border-tb-white"><td colspan="2">CONCEPTOS DE SU CUOTA DEL MES DE {selected_period}</td></tr>
+        <tr class="bg-black"><td class="border-tb-white">PRESUPUESTO TOTAL DEL MES:</td><td class="class="border-tb-white" text-right p-5">S/. {monthly_budget:.2f}</td></tr>
+        <tr class="bg-black"><td class="border-tb-white" colspan="2">CONCEPTOS DE SU CUOTA DEL MES DE {selected_period}</td></tr>
         <tr><td class="p-5 border-b">Cuota de mantenimiento:</td><td class="text-right p-5 border-b">S/. {maintenance_fee:.2f}</td></tr>
         <tr><td class="p-5 border-b">Cuota de Consumo de Agua Propio:</td><td class="text-right p-5 border-b">S/. {own_cost_with_tax:.2f}</td></tr>
         <tr><td class="p-5 border-b">Cuota Áreas Comunes y Fijo (inc. IGV):</td><td class="text-right p-5 border-b">S/. {common_cost_with_tax:.2f}</td></tr>
